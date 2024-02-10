0d5856fe9a4c514932b2b0287b676862ba6512aa
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-10 20:14:12 +0530

    Todo List is completed with all functionality

--- 07_TODO_JS/js/index.js
+++ 07_TODO_JS/js/index.js
@@ -1,9 +1,11 @@
 //  read from local storage
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-const todoData = document.getElementById("todoData");
+let listId = 0;
 const todoForm = document.getElementById("todoForm");
 const alertMsg = document.getElementById("alertMsg");
+const addTask = document.getElementById("addTask");
+const updateTask = document.getElementById("updateTask");
 
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -33,16 +35,43 @@ todoForm.addEventListener("submit", (e) => {
 });
 
 const btnChange = (v) => {
-    document.getElementById("addTask").style.display = v == 1 ? "block" : "none";
-    document.getElementById("updateTask").style.display = v == 1 ? "none" : "block";
+    addTask.style.display = v == 1 ? "block" : "none";
+    updateTask.style.display = v == 1 ? "none" : "block";
 }
 
 const editTodo = (id) => { 
     document.getElementById("taskName").value = todos[id - 1].name;
     document.getElementById("priority").value = todos[id - 1].priority;
 
+    listId = id;
+
     btnChange(2);
 }
+
+updateTask.addEventListener("click", (e) => { 
+    e.preventDefault();
+
+    const todoName = document.getElementById("taskName").value;
+    const todoPriority = document.getElementById("priority").value;
+
+    if (todoName === "") {
+        const d = `
+        <div class="alert alert-danger alert-dismissible fade show mb-4 role="alert">
+            <div class="text-center"><strong>Error!</strong> Please enter task name.</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`;
+        
+        alertMsg.innerHTML = d;
+        return;
+    }
+
+    todos[listId - 1].name = todoName;
+    todos[listId - 1].priority = todoPriority;
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderTodos();
+    todoForm.reset();
+});
 
 const deleteTodo = (id) => { 
     localStorage.setItem("todos", JSON.stringify(todos.filter((todo) => todo.id !== id)));
@@ -65,8 +94,8 @@ const renderTodos = () => {
             const prioritySpan = todo.priority === "High" ? "danger" : todo.priority === "Medium" ? "warning": "secondary";
             data += `
         <tr>
-            <td>${todo.name}</td>
-            <td><span class="badge bg-${prioritySpan}">${todo.priority}</span></td>
+            <td>${todo.name} <span class="badge bg-${prioritySpan}">${todo.priority}</span></td>
+
             <td>
                 <button class="btn btn-success" onclick="editTodo(${todo.id})"><i class="fa-solid fa-pen-to-square"></i></button>
                 <button class="btn btn-danger" onclick="deleteTodo(${todo.id})"><i class="fa-solid fa-trash"></i></button>

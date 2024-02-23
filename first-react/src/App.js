eab251e43d5f7f2b0d72b3bf9daa2aed82ee58fd
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-23 12:14:50 +0530

    mapping through a object

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./App.css";
 
-function MyButton(){
+function MyButton() {
   return (
     <>
-    <button>Click Here</button>
-    
+      <button>Click Here</button>
+
     </>
   );
 }
@@ -25,7 +25,13 @@ const App = () => {
     content = <MyButton />
   }
 
-  
+  // create list of courses map
+  const courses = [
+    { id: 1, name: "React", isElective: false },
+    { id: 2, name: "Node", isElective: true },
+    { id: 3, name: "MongoDB", isElective: false },
+    { id: 4, name: "Express", isElective: true },
+  ]
   return (
     <>
       <h1 className='heading'>Hello World</h1>
@@ -34,7 +40,18 @@ const App = () => {
       <img src={user.img} style={{ width: "200px", height: "200px" }} alt="random" />
       <br />
       <MyButton />
-      {content }
+      {content}
+      <br />
+
+      <ul>
+        {
+          courses.map((course) => (
+            <li key={course.id} style={{ color: course.isElective ? "blue" : "red" }}>
+              {course.name}
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }

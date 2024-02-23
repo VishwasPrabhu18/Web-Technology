f0026cecbc115dcbb2d53fd25e9971a12c0e99e3
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-23 12:06:15 +0530

    conditional rendering

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import "./App.css";
+
+function MyButton(){
+  return (
+    <>
+    <button>Click Here</button>
+    
+    </>
+  );
+}
 
 const App = () => {
+  // const name = "Vishwas Prabhu";
+  // create use map
+  const user = {
+    name: "Vishwas Prabhu",
+    age: 20,
+    address: "Udupi",
+    img: "https://i.imgur.com/yXOvdOSs.jpg"
+  }
+
+  let content;
+  if (user.age > 18) {
+    content = <MyButton />
+  }
+
+  
   return (
-    <div>App</div>
+    <>
+      <h1 className='heading'>Hello World</h1>
+      <h2>It is a nice day</h2>
+      <h1>My name is {user.name}</h1>
+      <img src={user.img} style={{ width: "200px", height: "200px" }} alt="random" />
+      <br />
+      <MyButton />
+      {content }
+    </>
   )
 }
 

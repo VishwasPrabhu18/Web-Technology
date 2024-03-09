c51033804e3d17097e8387051be604d128c1a930
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-03-09 20:42:15 +0530

    updated

---
+++ React_Working/first-react/src/SearchItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SearchItem = () => {
+
+  const category = [
+    { name: "Mango", price: 100, isVegitable: false },
+    { name: "Apple", price: 200, isVegitable: false },
+    { name: "Banana", price: 50, isVegitable: false },
+    { name: "Grapes", price: 150, isVegitable: false },
+    { name: "Brinjal", price: 100, isVegitable: true },
+    { name: "Tomato", price: 200, isVegitable: true },
+    { name: "Potato", price: 50, isVegitable: true },
+    { name: "Onion", price: 150, isVegitable: true },
+  ];
+
+  return (
+    <div>SearchItem</div>
+  )
+}
+
+export default SearchItem

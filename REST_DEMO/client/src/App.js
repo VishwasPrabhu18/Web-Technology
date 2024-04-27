711ba6da3342c978c6acf039f0a47d358b6b5c76
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-04-27 21:41:13 +0530

    Student List is added

--- REST_DEMO/client/src/App.js
+++ REST_DEMO/client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { StudentList } from './components/index'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <StudentList />
+    </div>
   )
 }
 
---
+++ REST_DEMO/client/src/components/Student.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Student = ({ stud, id }) => {
+  return (
+    <tr>
+      <td className='text-left px-6 py-4 whitespace-nowrap'>
+        <div className='text-base text-gray-500'>{id}</div>
+      </td>
+      <td className='text-left px-6 py-4 whitespace-nowrap'>
+        <div className='text-base text-gray-500'>{stud.name}</div>
+      </td>
+      <td className='text-left px-6 py-4 whitespace-nowrap'>
+        <div className='text-base text-gray-500'>{stud.rollNo}</div>
+      </td>
+      <td className='text-left px-6 py-4 whitespace-nowrap'>
+        <div className='text-base text-gray-500'>{stud.department}</div>
+      </td>
+      <td className='text-left px-6 py-4 whitespace-nowrap'>
+        <div className='text-base text-gray-500'>{stud.age}</div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap flex justify-center gap-3'>
+        <button type="button" class="px-5 py-1.5 text-sm font-medium text-white bg-green-500 hover:bg-green-600 rounded text-center">Edit</button>
+        <button type="button" class="px-5 py-1.5 text-sm font-medium text-white bg-red-500 hover:bg-red-600 rounded text-center">Delete</button>
+      </td>
+    </tr>
+  )
+}
+
+export default Student;

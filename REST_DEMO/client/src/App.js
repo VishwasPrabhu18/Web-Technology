f82077f898617a60d27ef54a7793fbd25133764e
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-05-01 12:24:06 +0530

    Student Mngt System project is completed

--- REST_DEMO/client/src/App.js
+++ REST_DEMO/client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import { AddStudent } from './components/index'
 
+import { Bounce, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <div className='container mx-auto  my-8 sm:px-56 px-0'>
+      <h1 className='text-center font-bold text-4xl text-blue-800 whitespace-nowrap py-2'>Student Management System</h1>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}>
+      </ToastContainer>
+
       <AddStudent />
     </div>
   )
